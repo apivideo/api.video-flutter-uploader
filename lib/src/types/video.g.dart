83ee743448076591747d42aa8d2996f666a78fb8
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-10-22 10:39:29 +0200

    feat(lib): update video class member nullability

--- lib/src/types/video.g.dart
+++ lib/src/types/video.g.dart
@@ -7,50 +7,41 @@ part of 'video.dart';
 // **************************************************************************
 
 Video _$VideoFromJson(Map<String, dynamic> json) => Video(
-      json['videoId'] as String,
-      createdAt: json['createdAt'] == null
-          ? null
-          : DateTime.parse(json['createdAt'] as String),
-      title: json['title'] as String?,
-      description: json['description'] as String?,
-      publishedAt: json['publishedAt'] as String?,
-      updatedAt: json['updatedAt'] == null
-          ? null
-          : DateTime.parse(json['updatedAt'] as String),
+      json['videoId'] as String, createdAt: DateTime.parse(json['createdAt'] as String),
+      title: json['title'] as String,
+      description: json['description'] as String,
+      publishedAt: json['publishedAt'] as String,
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
       discardedAt: json['discardedAt'] == null
           ? null
           : DateTime.parse(json['discardedAt'] as String),
       deletesAt: json['deletesAt'] == null
           ? null
           : DateTime.parse(json['deletesAt'] as String),
-      discarded: json['discarded'] as bool?,
+      discarded: json['discarded'] as bool,
       language: _$JsonConverterFromJson<String, Locale>(
           json['language'], const _JsonLocaleConverter().fromJson),
       languageOrigin:
           $enumDecodeNullable(_$LanguageOriginEnumMap, json['languageOrigin']),
-      tags: (json['tags'] as List<dynamic>?)?.map((e) => e as String).toList(),
-      metadata: (json['metadata'] as List<dynamic>?)
-          ?.map((e) => Metadata.fromJson(e as Map<String, dynamic>))
+      tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+      metadata: (json['metadata'] as List<dynamic>)
+          .map((e) => Metadata.fromJson(e as Map<String, dynamic>))
           .toList(),
-      source: json['source'] == null
-          ? null
-          : VideoSource.fromJson(json['source'] as Map<String, dynamic>),
-      assets: json['assets'] == null
-          ? null
-          : VideoAssets.fromJson(json['assets'] as Map<String, dynamic>),
+      source: VideoSource.fromJson(json['source'] as Map<String, dynamic>),
+      assets: VideoAssets.fromJson(json['assets'] as Map<String, dynamic>),
       playerId: json['playerId'] as String?,
-      public: json['_public'] as bool?,
-      panoramic: json['panoramic'] as bool?,
-      mp4Support: json['mp4Support'] as bool?,
+      public: json['public'] as bool,
+      panoramic: json['panoramic'] as bool,
+      mp4Support: json['mp4Support'] as bool,
     );
 
 Map<String, dynamic> _$VideoToJson(Video instance) => <String, dynamic>{
       'videoId': instance.videoId,
-      'createdAt': instance.createdAt?.toIso8601String(),
+      'createdAt': instance.createdAt.toIso8601String(),
       'title': instance.title,
       'description': instance.description,
       'publishedAt': instance.publishedAt,
-      'updatedAt': instance.updatedAt?.toIso8601String(),
+      'updatedAt': instance.updatedAt.toIso8601String(),
       'discardedAt': instance.discardedAt?.toIso8601String(),
       'deletesAt': instance.deletesAt?.toIso8601String(),
       'discarded': instance.discarded,
@@ -62,7 +53,7 @@ Map<String, dynamic> _$VideoToJson(Video instance) => <String, dynamic>{
       'source': instance.source,
       'assets': instance.assets,
       'playerId': instance.playerId,
-      '_public': instance.public,
+      'public': instance.public,
       'panoramic': instance.panoramic,
       'mp4Support': instance.mp4Support,
     };
